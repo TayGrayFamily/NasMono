eb638cb5c0d7bf68f0e70761d34fc055b7e96b7e
Taylor <taylorweil98@gmail.com>
2025-12-16 21:41:06 -0600

    add mono repo pnpm package

---
+++ packages/home/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+)
+  </StrictMode>,
+    <App />
+  <StrictMode>
+createRoot(document.getElementById('root')!).render(
+
+import App from './App.tsx'
+import './index.css'
+import { createRoot } from 'react-dom/client'
+
